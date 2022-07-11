67cad10e8a3d0ade9851e6b666f8b433ef3cb0f5
danail2003 <ddobrev20@outlook.com>
2022-07-11 18:53:47 +0300

    Added some components with HTML and CSS

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { AsideComponent } from './aside/aside.component';
+import { FooterComponent } from './footer/footer.component';
+import { ThemeListItemComponent } from './theme-list-item/theme-list-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AsideComponent,
+    FooterComponent,
+    ThemeListItemComponent
   ],
   imports: [
     BrowserModule,
