1264b788e73e2e85ea1dbc4862fc6984dcbb8c8c
danail2003 <ddobrev20@outlook.com>
2022-07-22 19:51:26 +0300

    Register validation is almost done

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { emailValidator, passwordMatch } from '../utils';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +8,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+  passwordFormControl = new FormControl('', [Validators.required, Validators.minLength(5)])
 
-  constructor() { }
+  get passwordGroup(): FormGroup {
+    return this.registerFormGroup.controls['passwords'] as FormGroup;
+  }
+
+  constructor(private formBuilder: FormBuilder) { }
+
+  registerFormGroup: FormGroup = this.formBuilder.group({
+    'username': new FormControl('', [Validators.required, Validators.minLength(5)]),
+    'email': new FormControl('', [Validators.required, emailValidator]),
+    'passwords': new FormGroup({
+      'password': this.passwordFormControl,
+      'rePassword': new FormControl('', passwordMatch(this.passwordFormControl))
+    }),
+    'tel': new FormControl(),
+    'telRegion': new FormControl()
+  })
 
   ngOnInit(): void {
   }
 
+  register(): void {
+
+  }
 }
--- src/app/auth/utils.ts
+++ src/app/auth/utils.ts
@@ -3,6 +3,10 @@ import { AbstractControl, ValidationErrors } from "@angular/forms";
 export function emailValidator(control: AbstractControl): ValidationErrors | null {
     const value = control.value;
 
+    if (!value) {
+        return null;
+    }
+
     if (!/.{6,}@gmail\.(com|bg)/.test(value)) {
         return {
             email: true
@@ -10,4 +14,16 @@ export function emailValidator(control: AbstractControl): ValidationErrors | nul
     }
 
     return null;
+}
+
+export function passwordMatch(passwordFormControl: AbstractControl) {
+    return (rePasswordFormControl: AbstractControl) => {
+        if (passwordFormControl.value !== rePasswordFormControl.value) {
+            return {
+                passwordMatch: true
+            }
+        }
+
+        return null;
+    }
 }
